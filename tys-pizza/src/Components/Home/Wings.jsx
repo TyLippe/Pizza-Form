14382757519c4849dec18422a75df86a859a79e0
Ty Lippe <tylippe94@gmail.com>
2020-06-01 13:44:12 -0400

    wing form sending correct data

--- tys-pizza/src/Components/Home/Wings.jsx
+++ tys-pizza/src/Components/Home/Wings.jsx
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import sauces from '../../Assets/Json/sauces.json'
 
 export default function WingForm() {
+    const [wings, setWings] = useState({
+        item_type: 'wings',
+        type: 'bone-in',
+        amount: '16',
+        sauce: 'naked'
+    })
+
     const handleChange = e => {
-        console.log(e.target.name)
+        e.persist()
+        console.log(e.target.name, e.target.value)
+        setWings({
+            ...wings,
+            [e.target.name]: e.target.value
+        })
     }
+
+    console.log(wings)
 
     return(
         <div className='wingDiv'>
             <h2>Wings</h2>
             <form className='wingForm'>
                 <p>Type</p>
-                <select onChange={handleChange}>
-                    <option value='boned'>Bone-In</option>
+                <select onChange={handleChange} name='type'>
+                    <option value='bone-in'>Bone-In</option>
                     <option value='boneless'>Boneless</option>
                 </select>
                 <p>Amount</p>
-                <select onChange={handleChange}>
+                <select onChange={handleChange} name='amount' defaultValue='16'>
                     <option value='8'>8 Count</option>
                     <option value='16'>16 Count</option>
                     <option value='24'>24 Count</option>
                 </select>
                 <p>Sauce</p>
+                <label>
+                    <input  
+                        id='toppingInput'
+                        type='radio'
+                        name='sauces'
+                        value='naked'
+                        defaultChecked='defaultChecked'
+                        onChange={handleChange}
+                    />
+                    Naked
+                </label>
                     {sauces.map(sauce => {
                         return(
                             <div>
@@ -31,7 +56,6 @@ export default function WingForm() {
                                         type='radio'
                                         name='sauces'
                                         value={sauce.value}
-                                        // checked={pizza.toppings.pepperoni}
                                         onChange={handleChange}
                                         />
                                     {sauce.name}
